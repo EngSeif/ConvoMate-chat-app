0353fd74c3af7dc3b539e32c14a8132fe5c9bb00
EngSeif <eng.seifmohamed2004@gmail.com>
2025-01-26 15:15:56 +0300

    Init Client Front And Added Public Images

---
+++ client-front/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com', 'randomuser.me'], // Allow images from Cloudinary
+    },
+};
+
+export default nextConfig;
